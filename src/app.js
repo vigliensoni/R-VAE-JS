84e9d7a7715620b10f83ba459d81a54bf2bed068
Gabriel Vigliensoni <gabriel@vigliensoni.com>
2020-07-13 15:24:35 -0400

    Add: Second MIDI output.

--- src/app.js
+++ src/app.js
@@ -43,8 +43,8 @@ let clock = new maxi.maxiClock()
 
 // control sequencer 
 const subdiv = 96 // 4 * 24 -> 1 beat
-const ticksperbeat = 24 // GV: why this is 12 and not 24?
-clock.setTempo(80) // Running at 160, though
+const ticksperbeat = 12 // GV: why this is 12 and not 24?
+clock.setTempo(160) // Running at 160, though
 clock.setTicksPerBeat(ticksperbeat)
 
 // console.log('clock', clock)
@@ -91,34 +91,46 @@ const playAudio = () => {
   maxiEngine.loadSample("https://raw.githubusercontent.com/vigliensoni/drum-sample-random-sequencer/master/audio/Rim%207T8.wav", snare);
   // maxiEngine.loadSample('./audio/ClosedHH 1.wav', hihat)
   maxiEngine.loadSample("https://raw.githubusercontent.com/vigliensoni/drum-sample-random-sequencer/master/audio/ClosedHH%201.wav", hihat);
-
+  
+  let w = 0;
   maxiEngine.play = function () {
-    var w = 0
-    clock.ticker()
+    
+    let tickCounter;
+    let beatCounter;
+    clock.ticker();
     if (clock.isTick()) {
       // let beatCounter = clock.playHead % 7;
-      let tickCounter = clock.playHead % subdiv
-      const beatCounter = Math.floor(clock.playHead / subdiv)
-      clockUI.innerHTML = (beatCounter + 1) + ' ' + Math.floor(tickCounter / ticksperbeat + 1) + ' ' + (tickCounter % ticksperbeat + 1) 
+      tickCounter = clock.playHead % subdiv;
+      beatCounter = Math.floor(clock.playHead / subdiv);
+      // clockUI.innerHTML = (beatCounter + 1) + ' ' + Math.floor(tickCounter / ticksperbeat + 1) + ' ' + (tickCounter % ticksperbeat + 1) 
             
-      vis.visualize(clock.playHead % LOOP_DURATION)
+      vis.visualize(tickCounter)
 
-      if (kkPat.indexOf(tickCounter) >= 0) {
-        if (kkMuted !== true) {
+      if ((kkPat.indexOf(tickCounter)) >= 0) {
+        if ((kkMuted !== true)) {
           kick.trigger()
-          if (webmidi) WebMidi.outputs[0].playNote("C1") // WebMidi not working on Firefox
+          if (webmidi) { 
+            WebMidi.outputs[0].playNote("C1"); // WebMidi not working on Firefox
+            WebMidi.outputs[1].playNote("C1"); // WebMidi not working on Firefox
+          }
         }
       }
-      if (snPat.indexOf(tickCounter) >= 0) {
-        if (snMuted !== true) {
+      if ((snPat.indexOf(tickCounter)) >= 0) {
+        if ((snMuted !== true)) {
           snare.trigger()
-          if (webmidi) WebMidi.outputs[0].playNote("A1")
+          if (webmidi)  {
+            WebMidi.outputs[0].playNote("A1");
+            WebMidi.outputs[1].playNote("A1");
+          }
         }
       }
-      if (hhPat.indexOf(tickCounter) >= 0) {
-        if (hhMuted !== true) {
+      if ((hhPat.indexOf(tickCounter) >= 0)) {
+        if ((hhMuted !== true)) {
           hihat.trigger()
-          if (webmidi) WebMidi.outputs[0].playNote("G#1")
+          if (webmidi) { 
+            WebMidi.outputs[0].playNote("G#1");
+            WebMidi.outputs[1].playNote("G#1");
+          }
         }
       }
     }
